126717372ba2ea7a5268ceea4f541be44b1d5287
abhineet <abhineetsingh192@gmail.com>
2025-12-16 06:05:59 +0530

    refactored

---
+++ ride_sharing/src/main/java/org/ride_sharing/App.java
@@ -0,0 +1,13 @@
+package org.ride_sharing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
