97ed948f8b3af501afe14ce0736f778b89c283c1
abhineet <abhineetsingh192@gmail.com>
2026-01-02 15:25:08 +0530

    refactored

--- bug_bounty/src/main/java/org/bug_bounty/App.java
+++ bug_bounty/src/main/java/org/bug_bounty/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        // assignee bug map and reporter bug map needs to be a list
+        // other bonus functionality
     }
 }
--- bug_bounty/src/main/java/org/bug_bounty/service/BugReportListingService.java
+++ bug_bounty/src/main/java/org/bug_bounty/service/BugReportListingService.java
@@ -1,6 +1,7 @@
 package org.bug_bounty.service;
 
 import lombok.Data;
+import org.bug_bounty.entity.BugReportStatus;
 
 @Data
 public class BugReportListingService {
@@ -15,17 +16,37 @@ public class BugReportListingService {
 
     public void listAllBugReports() {
         // Implementation to list all bug reports
+        bugReportingService.getBugReportMap().forEach((title, bugReport) -> {
+            System.out.println("ID: " + bugReport.getId() + ", Title: " + bugReport.getTitle() +
+                    ", Status: " + bugReport.getStatus() + ", Severity: " + bugReport.getSeverity());
+        });
     }
 
     public void listAssignedBugReports() {
         // Implementation to list assigned bug reports
+        bugReportingService.getAssigneeBugReportMap().forEach((assigneeEmail, bugReport) -> {
+            System.out.println("ID: " + bugReport.getId() + ", Title: " + bugReport.getTitle() +
+                    ", Status: " + bugReport.getStatus() + ", Severity: " + bugReport.getSeverity());
+        });
     }
 
     public void listAssignedAndCompletedBugReports() {
         // Implementation to list assigned and completed bug reports
+        bugReportingService.getAssigneeBugReportMap().forEach((assigneeEmail, bugReport) -> {
+            if (bugReport.getStatus() == BugReportStatus.CLOSED) {
+                System.out.println("ID: " + bugReport.getId() + ", Title: " + bugReport.getTitle() +
+                        ", Status: " + bugReport.getStatus() + ", Severity: " + bugReport.getSeverity());
+            }
+        });
     }
 
     public void listAssignedAndIncompleteBugReports() {
         // Implementation to list assigned and in-progress bug reports
+        bugReportingService.getAssigneeBugReportMap().forEach((assigneeEmail, bugReport) -> {
+            if (bugReport.getStatus() != BugReportStatus.CLOSED) {
+                System.out.println("ID: " + bugReport.getId() + ", Title: " + bugReport.getTitle() +
+                        ", Status: " + bugReport.getStatus() + ", Severity: " + bugReport.getSeverity());
+            }
+        });
     }
 }
