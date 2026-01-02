a41ac80b66fcbcfb99708ba2bf8cb89d33722e4f
abhineet <abhineetsingh192@gmail.com>
2026-01-02 15:42:53 +0530

    refactored

---
+++ lib_mgmt_system/src/main/java/org/lib_mgmt_system/App.java
@@ -0,0 +1,13 @@
+package org.lib_mgmt_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
